291da14dc8a959a865e2454632494e820c42274c
zhuolin <zhuolin1014@163.com>
2023-04-21 20:58:25 +0800

    todo

---
+++ cli.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+const program = require("commander");
+const api=require('./index.js')
+
+program
+    .option('-x, --xxx', 'what the x')
+
+program
+    .command('add')
+    .description('add a tack')
+    .action((...args) => {
+        const words = args.slice(0, -1).join(' ')
+        api.add(words).then(()=>{console.log('添加成功')},()=>{console.log('添加失败')})
+    })
+
+program
+    .command('clear')
+    .description('clear all tack')
+    .action(() => {
+        api.clear().then(()=>{'清除完毕'},()=>{'清除失败'})
+    })
+
+program.parse(process.argv);
+
+if(process.argv.length===2){
+    api.showAll()
+}
+
+
+
